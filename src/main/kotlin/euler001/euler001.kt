61eaca889658729e1f9f102064be6647b13516a9
Joey T <thechickenbane@gmail.com>
2015-12-21 16:57:55 -0800

    Completed Problem 1!

--- src/main/kotlin/euler001/euler001.kt
+++ src/main/kotlin/euler001/euler001.kt
@@ -1,5 +1,7 @@
 package euler001
 
+import java.util.*
+
 
 // Multiples of 3 and 5
 // Problem 1
@@ -10,4 +12,20 @@ package euler001
 fun Int.isMultiple3or5(): Boolean {
     if (this <= 0) return false
     return (this % 3 == 0) || (this % 5 == 0)
+}
+
+fun naturalsBelow(n: Int): List<Int> {
+    var i = 0
+    val list = ArrayList<Int>()
+    while (i < n) {
+        list.add(i)
+        i += 1
+    }
+    return list
+}
+
+val answer = naturalsBelow(1000).filter { it.isMultiple3or5() }.sum()
+
+fun main(args: Array<String>) {
+    println("my answer = $answer")
 }
--- src/test/kotlin/euler001/test001.kt
+++ src/test/kotlin/euler001/test001.kt
@@ -11,14 +11,26 @@ class Euler001 {
 
     // Is 0 a natural number?
     @Test fun check0() {
-        Assert.assertEquals("0 is not a multiple", false, 0.isMultiple3or5())
+        Assert.assertFalse("0 is not a multiple", 0.isMultiple3or5())
     }
     @Test fun check1to5() {
-        Assert.assertEquals("1 is not a multiple", false, 1.isMultiple3or5())
-        Assert.assertEquals("2 is not a multiple", false, 2.isMultiple3or5())
-        Assert.assertEquals("3 is a multiple", true, 3.isMultiple3or5())
-        Assert.assertEquals("4 is not a multiple", false, 4.isMultiple3or5())
-        Assert.assertEquals("5 is a multiple", true, 5.isMultiple3or5())
+        Assert.assertFalse("1 is not a multiple", 1.isMultiple3or5())
+        Assert.assertFalse("2 is not a multiple", 2.isMultiple3or5())
+        Assert.assertTrue("3 is a multiple", 3.isMultiple3or5())
+        Assert.assertFalse("4 is not a multiple", 4.isMultiple3or5())
+        Assert.assertTrue("5 is a multiple", 5.isMultiple3or5())
+    }
+
+    // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+    @Test fun checkDemo() {
+        val first10 = naturalsBelow(10)
+        Assert.assertEquals("naturalsBelow(10) should have 10 elements", 10, first10.size)
+
+        val multiples = first10.filter { it.isMultiple3or5() }
+        Assert.assertEquals("3, 5, 6, 9 are the multiples", listOf(3, 5, 6, 9), multiples)
+
+        Assert.assertEquals("sum correct", 23, multiples.sum())
     }
 }
 
