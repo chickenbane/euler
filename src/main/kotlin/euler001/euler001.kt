94e13af17073cdc9941b3e8cb7bdbe4842909947
Joey T <thechickenbane@gmail.com>
2015-12-21 16:35:23 -0800

    Why .idea keeps changing idk

--- src/main/kotlin/euler001/euler001.kt
+++ src/main/kotlin/euler001/euler001.kt
@@ -6,19 +6,8 @@ package euler001
 // If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
 // Find the sum of all the multiples of 3 or 5 below 1000.
 
-class Euler001 {
 
-    //fun Int.IsMultiple3or5(): Boolean = (this % 3 == 0) || (this % 5 == 0)
-
-}
-
-fun Int.isMultiple3or5(): Boolean = (this % 3 == 0) || (this % 5 == 0)
-
-fun herro(name: String) {
-    println("oh hai $name")
-}
-
-
-fun main(args: Array<String>) {
-    //println("euler 001")
+fun Int.isMultiple3or5(): Boolean {
+    if (this <= 0) return false
+    return (this % 3 == 0) || (this % 5 == 0)
 }
--- src/test/kotlin/euler001/test001.kt
+++ src/test/kotlin/euler001/test001.kt
@@ -2,17 +2,23 @@ package euler001
 
 import org.junit.Assert
 import org.junit.Test
-import euler001.isMultiple3or5
 
 class Euler001 {
-    @Test fun testIs3or5Multiple() {
-        Assert.assertEquals("please work.", true, true)
-        herro("kotlin")
 
+    // first implementation
+    //      fun Int.isMultiple3or5(): Boolean = (this % 3 == 0) || (this % 5 == 0)
+    // will return true for 0
+
+    // Is 0 a natural number?
+    @Test fun check0() {
+        Assert.assertEquals("0 is not a multiple", false, 0.isMultiple3or5())
     }
-
-    @Test fun needNamedTest() {
-        Assert.assertEquals("test name", true, 3.isMultiple3or5())
+    @Test fun check1to5() {
+        Assert.assertEquals("1 is not a multiple", false, 1.isMultiple3or5())
+        Assert.assertEquals("2 is not a multiple", false, 2.isMultiple3or5())
+        Assert.assertEquals("3 is a multiple", true, 3.isMultiple3or5())
+        Assert.assertEquals("4 is not a multiple", false, 4.isMultiple3or5())
+        Assert.assertEquals("5 is a multiple", true, 5.isMultiple3or5())
     }
 }
 
