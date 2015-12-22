c78d94b8445accf47341f327a4dcd19af6ebcd54
Joey T <thechickenbane@gmail.com>
2015-12-21 16:02:35 -0800

    hurray, junit works.  finally.

---
+++ src/main/kotlin/euler001/euler001.kt
@@ -0,0 +1,17 @@
+package euler001
+
+
+// Multiples of 3 and 5
+// Problem 1
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+// Find the sum of all the multiples of 3 or 5 below 1000.
+
+class Euler001 {
+
+}
+
+fun Int.canDivideBy3or5(): Boolean = (this % 3 == 0) || (this % 5 == 0)
+
+fun main(args: Array<String>) {
+    //println("euler 001")
+}
---
+++ src/test/kotlin/euler001/test001.kt
@@ -0,0 +1,10 @@
+package euler001
+
+import org.junit.Assert
+import org.junit.Test
+
+class Euler001 {
+    @Test public fun testIs3or5Multiple() {
+        Assert.assertEquals(true, false)
+    }
+}
