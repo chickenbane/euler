8eacba218bd2721e24cb4107a2981c81086caf68
Joey T <thechickenbane@gmail.com>
2015-12-25 09:57:56 -0800

    completed 6, easy

--- src/main/kotlin/euler00x/euler006.kt
+++ src/main/kotlin/euler00x/euler006.kt
@@ -11,3 +11,18 @@ package euler00x
 //Hence the difference between the sum of the squares of the first ten natural numbers and the square of the sum is 3025 − 385 = 2640.
 //
 //Find the difference between the sum of the squares of the first one hundred natural numbers and the square of the sum.
+
+fun sumOfSquares(top: Int): Int {
+    return (1..top).toList().map { it * it }.sum()
+}
+
+fun squareOfSum(top: Int): Int {
+    val sum = (1..top).toList().sum()
+    return sum * sum
+}
+
+fun diffSumSquaresAndSquareSums(top: Int): Int = squareOfSum(top) - sumOfSquares(top)
+
+fun main(args: Array<String>) {
+    println("my answer = ${diffSumSquaresAndSquareSums(100)}")
+}
