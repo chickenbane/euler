ace1d0dcbe0edfaade1d6519e2d351ef6b2b7786
Joey T <thechickenbane@gmail.com>
2016-02-20 15:14:20 -0800

    feel better

--- src/main/kotlin/euler03x/Euler039.kt
+++ src/main/kotlin/euler03x/Euler039.kt
@@ -16,32 +16,6 @@ If p is the perimeter of a right angle triangle with integral length sides, {a,b
 For which value of p ≤ 1000, is the number of solutions maximised?
 """
 
-    // advantages over Triple<Int, Int, Int>
-    // 1: Don't want to type Triple<Int, Int, Int>
-    // 2: lol, while "avoiding" writing that I've written it twice
-    // 3: a, b, c and much easier to unpack
-    // 4: it's fine.
-    data class Tri(val a: Int, val b: Int, val c: Int) {
-        // god i love the pythagorean theorem
-        val isRightTriangle: Boolean = (a*a + b*b) == c*c
-        val perimeter = a + b + c
-    }
-
-    fun findSolutions(perimeter: Int): List<Tri> {
-        val list = ArrayList<Tri>()
-        for (a in 1..perimeter) {
-            for (b in 1..perimeter) {
-                for (c in 1..perimeter) {
-                    val t = Tri(a, b, c)
-                    if (t.perimeter == perimeter && t.isRightTriangle) {
-                        list.add(t)
-                    }
-                }
-            }
-        }
-        return list
-    }
-
     fun answer(): Int {
         var max = 0
         var maxNum = 0
@@ -56,18 +30,17 @@ For which value of p ≤ 1000, is the number of solutions maximised?
         return maxNum
     }
 
-    // this brute force is doing worse than normal brute force.  i'll reach p = 1000, but this will run much longer than 1 minute
-    // fine, no objects.
-    // also, defect, a tri(3,4,5) should be the same as tri(4,3,5)
-    fun findSolutions2(perimeter: Int): Set<Triple<Int, Int, Int>> {
-        val set = LinkedHashSet<Triple<Int, Int, Int>>()
+    data class RightTriangle(val a: Int, val b: Int, val c: Int)
+
+    fun findSolutions2(perimeter: Int): Set<RightTriangle> {
+        val set = LinkedHashSet<RightTriangle>()
         for (a in 1..perimeter - 1) {
             for (b in 1..perimeter - 1) {
                 val c = perimeter - a - b
                 val right = (a*a + b*b) == c*c
                 if (!right) continue
-                val abc = Triple(a, b, c)
-                val bac = Triple(b, a, c)
+                val abc = RightTriangle(a, b, c)
+                val bac = RightTriangle(b, a, c)
                 if (abc !in set && bac !in set) set.add(abc)
             }
         }
--- src/test/kotlin/euler03x/Euler039Test.kt
+++ src/test/kotlin/euler03x/Euler039Test.kt
@@ -9,6 +9,20 @@ import org.junit.Test
  */
 class Euler039Test {
     @Test
+    fun example() {
+        /*
+        If p is the perimeter of a right angle triangle with integral length sides, {a,b,c}, there are exactly three solutions for p = 120.
+
+        {20,48,52}, {24,45,51}, {30,40,50}
+        */
+        val ex = Euler039.findSolutions2(120)
+        Assert.assertEquals("3 solutions for p = 120", 3, ex.size)
+
+        val tri345 = Euler039.RightTriangle(30, 40, 50)
+        Assert.assertTrue("{30, 40, 50} a solution for p = 120", tri345 in ex)
+    }
+
+    @Test
     fun answer() {
         Assert.assertEquals("my answer", 840, Euler039.answer())
     }
